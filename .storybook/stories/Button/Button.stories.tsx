b8bd090c2002ce8cdab3cd2f8cd48671eff44a7e
Qodestackr <winchygichu@gmail.com>
2023-08-05 20:52:08 +0300

    --

--- .storybook/stories/Button/Button.stories.tsx
+++ .storybook/stories/Button/Button.stories.tsx
@@ -1,33 +1,18 @@
+import Button from "../../../src/core/components/button/Button";
 import type { Meta, StoryObj } from "@storybook/react";
-import Button from '../../../src/core/components/button/Button'
-// Import Button Component
 
 const meta: Meta<typeof Button> = {
-  title: "@Button",
   component: Button,
 };
 
-// Story
-
+export default meta;
 type Story = StoryObj<typeof Button>;
 
-/**
- * Each named export is a story. Its contents specify how the story is rendered in addition
- * to other configuration options.
+/*
+ *👇 Render functions are a framework specific feature to allow you control on how the component renders.
+ * See https://storybook.js.org/docs/react/api/csf
+ * to learn how to use render functions.
  */
-
 export const Primary: Story = {
-  args: {
-    // primary: true,
-    // label: "Display Button",
-    // backgroundColor: "blue", // change to design system's match
-  },
+  render: () => <Button primary label="Button" />,
 };
-
-// export const Warning: Story = {
-//   args: {
-//     primary: true,
-//     label: "Delete now",
-//     backgroundColor: "red",
-//   },
-// };
--- src/core/components/button/Button.tsx
+++ src/core/components/button/Button.tsx
@@ -1,7 +1,6 @@
 import React, { ButtonHTMLAttributes, AnchorHTMLAttributes } from "react";
 import clsx from "clsx";
-// import { Button } from "@/components/ui/button";
-import { cn } from "../../utils/styles/classnames";
+import { cn } from "@core/utils/styles/classnames";
 
 type BaseButtonProps = {
   variant?: "neutral" | "primary" | "secondary" | "accent" | "ghost" | "link";
@@ -24,27 +23,24 @@ function Button<T extends React.ElementType = "button">({
   ...props
 }: PolymorphicButtonProps<T>) {
   const baseStyles = clsx(
-    "px-4", // Base padding-x class from daisyui
-    "py-2", // Base padding-y class from daisyui
-    "rounded", // Base border-radius class from daisyui
-    "focus:outline-none", // Apply focus outline removal on focus
-    fullWidth && "w-full", // Make the button full width if fullWidth is true
-    // Add variant-specific classes based on the 'variant' prop
+    "px-4",
+    "py-2",
+    "rounded",
+    "focus:outline-none",
+    fullWidth && "w-full",
+
     `btn${active ? "-active" : ""}`,
     variant === "neutral" ? "" : `btn-${variant}`,
-    // Add size-specific classes based on the 'size' prop
     size === "sm" ? "text-sm" : size === "lg" ? "text-lg" : "text-base",
-    className // Any custom class passed from props
+    className
   );
 
   // Determine if the component is an anchor ('a') or a button ('button')
   const isAnchor = Component === "a";
 
-  // Render the component accordingly with the appropriate props
   return (
     <Component
       className={baseStyles}
-      // Add 'href' attribute for anchors and 'type' attribute for buttons
       {...(isAnchor
         ? { href: (props as AnchorHTMLAttributes<T>).href }
         : { type: (props as ButtonHTMLAttributes<T>).type })}
