fed18e136d1e54989cb61f742fd417d96423aac9
Qodestackr <winchygichu@gmail.com>
2023-08-23 10:39:11 +0300

    Fix typo

--- src/context/_base_types/auth/auth.ts
+++ src/context/_base_types/auth/auth.ts
@@ -1,5 +1,5 @@
 // Authentication
-export const REGICTER = "REGISTER";
+export const REGISTER = "REGISTER";
 export const REGISTER_LOADING = "REGISTER_LOADING";
 export const REGISTER_SUCCESS = "REGISTER_SUCCESS";
 export const REGISTER_FAILED = "REGISTER_FAILED";
